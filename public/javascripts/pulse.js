5099c968a068470cd0b03595dfc0f4b9d93708e2
Mark McGranaghan <mmcgrana@gmail.com>
2011-02-05 01:17:01 -0800

    generalize

--- public/javascripts/pulse.js
+++ public/javascripts/pulse.js
@@ -1,46 +1,37 @@
-function pulseInit() {
-  var buffer_size = 60;
-  var events_per_second_buffer = [];
-  var nginx_requests_per_second_buffer = [];
+function pulse_init() {
+  var sparkline_opts = {chartRangeMin: 0, spotColor: false, minSpotColor: false, maxSpotColor: false, width: 180, height: 60};
+  var buffer_size = 90;
+  var buffers = {};
 
   if (!window.WebSocket) {
     alert("WebSocket not supported by this browser!");
   } else {
     var ws = new WebSocket("ws://localhost:8080/stats");
-    ws.onopen = function() {
-      console.log("websocket open");
-    };
-    ws.onclose = function() {
-      console.log("websocket close");
-    };
-    ws.onerror = function(e) {
-      console.log("websocket error error='" + e + "'");
-    };
+    ws.onopen =  function()  { console.log("websocket open"); };
+    ws.onclose = function()  { console.log("websocket close") };
+    ws.onerror = function(e) { console.log("websocket error error='" + e + "'"); };
     ws.onmessage = function(m) {
       if (m.data) {
         var stat = JSON.parse(m.data);
         var stat_key = stat[0];
         var stat_val = stat[1];
         console.log("websocket message stat_key=" + stat_key);
-        if (stat_key == "events_per_second") {
-          if (events_per_second_buffer.length == buffer_size) {
-            events_per_second_buffer.shift();
+        var scalar_id = "#" + stat_key + "_scalar";
+        var sparkline_id = "#" + stat_key + "_sparkline";
+        if ($(scalar_id) != null) {
+          if (buffers[stat_key] == null) {
+            buffers[stat_key] = [];
           }
-          events_per_second_buffer.push(stat_val);
-          $("#events_per_second_scalar").html(stat_val);
-          $("#events_per_second_sparkline").sparkline(events_per_second_buffer, {chartRangeMin: 0, spotColor: false, minSpotColor: false, maxSpotColor: false, width: 100, height: 60});
-        }
-        if (stat_key == "nginx_requests_per_second") {
-          if (nginx_requests_per_second_buffer.length == buffer_size) {
-            nginx_requests_per_second_buffer.shift();
+          if (buffers[stat_key].length == buffer_size) {
+            buffers[stat_key].shift();
           }
-          nginx_requests_per_second_buffer.push(stat_val);
-          $("#nginx_requests_per_second_scalar").html(stat_val);
-          $("#nginx_requests_per_second_sparkline").sparkline(nginx_requests_per_second_buffer, {chartRangeMin: 0, spotColor: false, minSpotColor: false, maxSpotColor: false, width: 100, height: 60});
+          buffers[stat_key].push(stat_val);
+          $(sparkline_id).sparkline(buffers[stat_key], sparkline_opts);
+          $(scalar_id).html(stat_val);
         }
       }
     };
   }
 }
 
-$(document).ready(pulseInit);
+$(document).ready(pulse_init);
