5f6479f833eb34d6a0b2498589ee46fba377e5b3
Mark McGranaghan <mmcgrana@gmail.com>
2011-02-08 22:36:47 -0800

    clojure buffers

--- public/javascripts/pulse.js
+++ public/javascripts/pulse.js
@@ -1,7 +1,5 @@
 function pulse_init() {
   var sparkline_opts = {chartRangeMin: 0, spotColor: false, minSpotColor: false, maxSpotColor: false, width: 150, height: 60};
-  var buffer_size = 100;
-  var buffers = {};
 
   if (!window.WebSocket) {
     alert("WebSocket not supported by this browser!");
@@ -14,19 +12,12 @@ function pulse_init() {
       if (m.data) {
         var stat = JSON.parse(m.data);
         var stat_key = stat[0];
-        var stat_val = stat[1];
-        console.log("websocket message stat_key=" + stat_key);
+        var stat_buff = stat[1];
+        var stat_val = stat_buff[stat_buff.length - 1];
         var scalar_id = "#" + stat_key + "_scalar";
         var sparkline_id = "#" + stat_key + "_sparkline";
         if ($(scalar_id) != null) {
-          if (buffers[stat_key] == null) {
-            buffers[stat_key] = [];
-          }
-          if (buffers[stat_key].length == buffer_size) {
-            buffers[stat_key].shift();
-          }
-          buffers[stat_key].push(stat_val);
-          $(sparkline_id).sparkline(buffers[stat_key], sparkline_opts);
+          $(sparkline_id).sparkline(stat_buff, sparkline_opts);
           $(scalar_id).html(stat_val);
         }
       }
