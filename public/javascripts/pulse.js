91ed3c9585348e0579a9f9611d0bfe4a784ee4e9
Mark McGranaghan <mmcgrana@gmail.com>
2011-06-28 23:34:09 -0700

    web

--- public/javascripts/pulse.js
+++ public/javascripts/pulse.js
@@ -1,28 +1,23 @@
-function pulse_init() {
-  var sparkline_opts = {chartRangeMin: 0, spotColor: false, minSpotColor: false, maxSpotColor: false, width: 150, height: 60};
+function pulseUpdate() {
+  var sparklineOpts = {chartRangeMin: 0, spotColor: false, minSpotColor: false, maxSpotColor: false, width: 150, height: 60};
 
-  if (!window.WebSocket) {
-    alert("WebSocket not supported by this browser!");
-  } else {
-    var ws = new WebSocket(pulse_websocket_url);
-    ws.onopen =  function()  { console.log("websocket open"); };
-    ws.onclose = function()  { console.log("websocket close") };
-    ws.onerror = function(e) { console.log("websocket error error='" + e + "'"); };
-    ws.onmessage = function(m) {
-      if (m.data) {
-        var stat = JSON.parse(m.data);
-        var stat_key = stat[0];
-        var stat_buff = stat[1];
-        var stat_val = stat_buff[stat_buff.length - 1];
-        var scalar_id = "#" + stat_key + "_scalar";
-        var sparkline_id = "#" + stat_key + "_sparkline";
-        if ($(scalar_id) != null) {
-          $(sparkline_id).sparkline(stat_buff, sparkline_opts);
-          $(scalar_id).html(stat_val);
-        }
+  $.get("/stats", function(stats) {
+    for (var statName in stats) {
+      var statBuff = stats[statName];
+      var statVal = statBuff[statBuff.length - 1];
+      var scalarId = "#" + statName + "-scalar";
+      var sparklineId = "#" + statName + "-sparkline";
+      if ($(scalarId) != null) {
+        $(sparklineId).sparkline(statBuff, sparklineOpts);
+        $(scalarId).html(Math.round(statVal));
       }
-    };
-  }
+    }
+  });
 }
 
-$(document).ready(pulse_init);
+function pulseInit() {
+  pulseUpdate();
+  setInterval(pulseUpdate, 500);
+}
+
+$(document).ready(pulseInit);
